33ab517762cd26fd5aecf87cbd8cb05555175dc9
Вадим Крамаренко <litlmayn@mail.ru>
2023-04-15 20:25:13 +0300

    v 1.1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'p&l%385148kslhtyn^##a1)itz@4zqj=rq&agdol^##zgl8(vd')
 
 DEBUG = False
 
